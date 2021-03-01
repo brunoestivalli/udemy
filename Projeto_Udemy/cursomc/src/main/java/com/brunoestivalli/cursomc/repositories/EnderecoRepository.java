7a49a8ab5b5105d16716a93169ecdeb510379bc8
brunoestivalli <bruno.estivalli@gmail.com>
2021-03-01 10:45:36 -0300

    Cliente, TipoCliente, Telefones e endereços

---
+++ Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,16 @@
+package com.brunoestivalli.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import com.brunoestivalli.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+	
+	
+	
+	
+
+}
