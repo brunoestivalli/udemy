b4ec515d418827492905592a034f41a0cea4fc31
brunoestivalli <bruno.estivalli@gmail.com>
2021-03-02 11:48:26 -0300

    Inserindo uma nova Categoria com POST

--- Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/resources/CategoriaResource.java
+++ Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.brunoestivalli.cursomc.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brunoestivalli.cursomc.domain.Categoria;
 import com.brunoestivalli.cursomc.services.CategoriaService;
@@ -25,4 +29,12 @@ public class CategoriaResource {
 		
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
