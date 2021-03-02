4d3c2b1732e0fd7ce8e07ecb24c5257396783e08
brunoestivalli <bruno.estivalli@gmail.com>
2021-03-02 11:19:00 -0300

    Atualização: utilizando somente JsonIgnore

--- Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/domain/Categoria.java
+++ Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/domain/Categoria.java
@@ -1,6 +1,7 @@
 package com.brunoestivalli.cursomc.domain;
 
 import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,8 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Categoria implements Serializable{
@@ -21,7 +20,7 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
+	
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
