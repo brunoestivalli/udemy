1a09d42d26ba3b13d7dd08fbd093b448733c0325
brunoestivalli <bruno.estivalli@gmail.com>
2021-03-03 11:37:22 -0300

    Validação sintática com Bean Validation

--- Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/services/CategoriaService.java
+++ Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.brunoestivalli.cursomc.domain.Categoria;
+import com.brunoestivalli.cursomc.dto.CategoriaDTO;
 import com.brunoestivalli.cursomc.repositories.CategoriaRepository;
 import com.brunoestivalli.cursomc.services.exceptions.DataIntegrityException;
 import com.brunoestivalli.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 
 }
