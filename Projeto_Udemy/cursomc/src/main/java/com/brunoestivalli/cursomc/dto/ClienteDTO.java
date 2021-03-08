71804d6d82ea483f4ce3c8afe0870238bf9b19af
brunoestivalli <bruno.estivalli@gmail.com>
2021-03-08 09:45:25 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/dto/ClienteDTO.java
+++ Projeto_Udemy/cursomc/src/main/java/com/brunoestivalli/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.brunoestivalli.cursomc.domain.Cliente;
+import com.brunoestivalli.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
